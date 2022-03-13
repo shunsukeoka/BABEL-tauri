24c6411f52ae27f31530e3ec943a94ce21a85756
Shunsuke Oka <dev@5hun.jp>
2022-03-13 20:36:08 +0900

    feat: ファイルリスト用コンポーネントを追加 #11

--- src/App.svelte
+++ src/App.svelte
@@ -1,20 +1,29 @@
 <script lang="ts">
     import { dialog, tauri } from '@tauri-apps/api';
     import Button from './lib/base/Button.svelte';
+    import FileList from './lib/FileList.svelte';
+    import type { IFIleInfo } from './interfaces/file';
+
+    let fileList: IFIleInfo[] = [];
 
     const onClickButton = async () => {
         const path = await dialog.open({ directory: true });
 
         if (!path) return;
 
-        const response = await tauri.invoke('get_directory_info', { path });
+        const { success, data } = await tauri.invoke('get_directory_info', { path });
 
-        console.log(response);
+        if (success) fileList = data;
+    };
+
+    const onClickFileListItem = ({ detail }) => {
+        console.log(detail.item);
     };
 </script>
 
 <main>
     <Button text="Button" on:click={onClickButton} />
+    <FileList items={fileList} on:clickItem={onClickFileListItem} />
 </main>
 
 <style lang="scss">
