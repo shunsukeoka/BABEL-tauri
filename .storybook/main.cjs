d799164b6aca2e93c8d2772c70eba10c00396915
Shunsuke Oka <dev@5hun.jp>
2022-03-14 21:46:11 +0900

    feat: storybookのviteの設定をルートの設定ファイルを読み込むように変更

--- .storybook/main.cjs
+++ .storybook/main.cjs
@@ -1,3 +1,6 @@
+const path = require('path');
+const { loadConfigFromFile, mergeConfig } = require('vite');
+
 module.exports = {
     stories: ['../src/**/*.stories.mdx', '../src/**/*.stories.@(js|jsx|ts|tsx|svelte)'],
     addons: ['@storybook/addon-links', '@storybook/addon-essentials', '@storybook/addon-svelte-csf'],
@@ -7,5 +10,13 @@ module.exports = {
     },
     svelteOptions: {
         preprocess: require('../svelte.config.cjs').preprocess,
+    },
+    async viteFinal(config, { configType }) {
+        const { config: userConfig } = await loadConfigFromFile(path.resolve(__dirname, '../vite.config.ts'));
+
+        return mergeConfig(config, {
+            ...userConfig,
+            plugins: [],
+        });
     },
 };
