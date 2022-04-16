fbc6b3634cb0c111b2edcd2b990a2520e5868518
Shunsuke Oka <dev@5hun.jp>
2022-04-17 01:18:14 +0900

    chore: 不要な記述を削除

--- src/components/extensions.d.ts
+++ src/components/extensions.d.ts
@@ -5,8 +5,5 @@ declare namespace JSX {
         'sp-sidenav': any
         'sp-sidenav-item': any
         'sp-sidenav-heading': any
-        'sp-icon': any
-        'sp-icon-arrow': any
-        'sp-icon-star': any
     }
 }
