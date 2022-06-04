5069218f9f6b0749cff6b7e70307e2dc4072333b
Shunsuke Oka <shunsukeoka@ShunsukenoMacBook-Air.local>
2022-06-04 14:50:52 +0900

    feat: パッケージ管理ツールの導入とフォーマット設定

--- src-tauri/src/command/file.rs
+++ src-tauri/src/command/file.rs
@@ -8,62 +8,62 @@ use thiserror::Error;
 
 #[derive(Debug, Error)]
 enum FileInfoError {
-    #[error("ディレクトリの読み込みに失敗しました。: {0}")]
-    ReadDirectoryError(String),
+	#[error("ディレクトリの読み込みに失敗しました。: {0}")]
+	ReadDirectoryError(String),
 
-    #[error("メタデータの読み込みに失敗しました。")]
-    ReadMetadataError(),
+	#[error("メタデータの読み込みに失敗しました。")]
+	ReadMetadataError(),
 }
 
 pub fn read_directory(path: &String, file_info_vec: &mut Vec<FileInfo>) -> Result<()> {
-    let entries =
-        fs::read_dir(path).context(FileInfoError::ReadDirectoryError(path.to_string()))?;
+	let entries =
+		fs::read_dir(path).context(FileInfoError::ReadDirectoryError(path.to_string()))?;
 
-    for entry in entries {
-        if let Ok(entry) = entry {
-            let entry_ptr = Rc::new(&entry);
+	for entry in entries {
+		if let Ok(entry) = entry {
+			let entry_ptr = Rc::new(&entry);
 
-            let metadata = entry_ptr
-                .metadata()
-                .context(FileInfoError::ReadMetadataError())?;
+			let metadata = entry_ptr
+				.metadata()
+				.context(FileInfoError::ReadMetadataError())?;
 
-            let file_info = FileInfo {
-                file_path: entry_ptr.path(),
-                file_name: entry_ptr
-                    .file_name()
-                    .into_string()
-                    .unwrap_or("".to_string()),
-                file_size: metadata.len(),
-                mime: mime_guess::from_path(entry_ptr.path())
-                    .first_raw()
-                    .unwrap_or("")
-                    .to_string(),
-                is_dir: metadata.is_dir(),
-                is_file: metadata.is_file(),
-                is_symlink: metadata.is_symlink(),
-                readonly: metadata.permissions().readonly(),
-                created_t: systemtime2string(
-                    metadata
-                        .created()
-                        .context(FileInfoError::ReadMetadataError())?,
-                ),
-                modified_t: systemtime2string(
-                    metadata
-                        .modified()
-                        .context(FileInfoError::ReadMetadataError())?,
-                ),
-                accessed_t: systemtime2string(
-                    metadata
-                        .accessed()
-                        .context(FileInfoError::ReadMetadataError())?,
-                ),
-            };
+			let file_info = FileInfo {
+				file_path: entry_ptr.path(),
+				file_name: entry_ptr
+					.file_name()
+					.into_string()
+					.unwrap_or("".to_string()),
+				file_size: metadata.len(),
+				mime: mime_guess::from_path(entry_ptr.path())
+					.first_raw()
+					.unwrap_or("")
+					.to_string(),
+				is_dir: metadata.is_dir(),
+				is_file: metadata.is_file(),
+				is_symlink: metadata.is_symlink(),
+				readonly: metadata.permissions().readonly(),
+				created_t: systemtime2string(
+					metadata
+						.created()
+						.context(FileInfoError::ReadMetadataError())?,
+				),
+				modified_t: systemtime2string(
+					metadata
+						.modified()
+						.context(FileInfoError::ReadMetadataError())?,
+				),
+				accessed_t: systemtime2string(
+					metadata
+						.accessed()
+						.context(FileInfoError::ReadMetadataError())?,
+				),
+			};
 
-            file_info_vec.push(file_info);
-        } else {
-            Err(FileInfoError::ReadDirectoryError(path.to_string()))?;
-        }
-    }
+			file_info_vec.push(file_info);
+		} else {
+			Err(FileInfoError::ReadDirectoryError(path.to_string()))?;
+		}
+	}
 
-    Ok(())
+	Ok(())
 }
--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -1,6 +1,6 @@
 #![cfg_attr(
-    all(not(debug_assertions), target_os = "windows"),
-    windows_subsystem = "windows"
+	all(not(debug_assertions), target_os = "windows"),
+	windows_subsystem = "windows"
 )]
 
 mod command;
@@ -9,10 +9,10 @@ mod helper;
 mod model;
 
 fn main() {
-    tauri::Builder::default()
-        .invoke_handler(tauri::generate_handler![
-            handler::files_event_handler::get_directory_info
-        ])
-        .run(tauri::generate_context!())
-        .expect("error while running tauri application");
+	tauri::Builder::default()
+		.invoke_handler(tauri::generate_handler![
+			handler::files_event_handler::get_directory_info
+		])
+		.run(tauri::generate_context!())
+		.expect("error while running tauri application");
 }
