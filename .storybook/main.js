ec4ca5603c95e95e873a42b7bca0da4254d83f30
Shunsuke Oka <dev@5hun.jp>
2022-06-15 04:18:36 +0900

    chore: update packages

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,8 +1,5 @@
 const path = require('path')
-
 const { loadConfigFromFile, mergeConfig } = require('vite')
-
-const svgrPlugin = require('vite-plugin-svgr')
 
 module.exports = {
     stories: ['../src/**/*.stories.mdx', '../src/**/*.stories.@(js|jsx|ts|tsx)'],
@@ -14,7 +11,6 @@ module.exports = {
 
     async viteFinal(config, { configType }) {
         const { config: userConfig } = await loadConfigFromFile(path.resolve(__dirname, '../vite.config.ts'))
-        console.log(path.resolve(__dirname, '../vite.config.ts'))
 
         if (process.env.NODE_ENV === 'production') {
             config.build.chunkSizeWarningLimit = 1200
@@ -22,13 +18,6 @@ module.exports = {
 
         return mergeConfig(config, {
             ...userConfig,
-            plugins: [
-                svgrPlugin({
-                    svgrOptions: {
-                        icon: true,
-                    },
-                }),
-            ],
             define: process.env.NODE_ENV === 'development' ? { ...config.define, global: 'window' } : undefined,
         })
     },
