23dc4cbb15bbd06fd5e06e996b3104301d930bbd
Shunsuke Oka <dev@5hun.jp>
2022-06-26 20:17:24 +0900

    feat: stateを追加

--- src-tauri/src/handler/audio_playback_handler.rs
+++ src-tauri/src/handler/audio_playback_handler.rs
@@ -1,14 +1,14 @@
-use crate::{AudioPlaybackState, AudioState};
+use crate::{AppState, AudioCommands};
 
 #[tauri::command]
-pub fn audio_play(
-	audio_event_sender: tauri::State<crossbeam_channel::Sender<AudioState>>,
-	path: String,
-) -> anyhow::Result<(), String> {
-	let _ = audio_event_sender.send(AudioState {
-		playback_state: AudioPlaybackState::LOADING,
-		current_file_path: path,
-	});
+pub fn audio_play(state: tauri::State<AppState>, path: String) -> anyhow::Result<(), String> {
+	let mut audio_state = state.audio_state.lock().unwrap();
+
+	audio_state.current_file = Some(std::fs::File::open(path).unwrap());
+
+	let _ = audio_state
+		.command_sender
+		.send(AudioCommands::PlayAudioFile);
 
 	Ok(())
 }
--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -4,12 +4,33 @@
 )]
 
 use rodio::cpal::traits::HostTrait;
-use std::io::BufReader;
+use std::{
+	fs::File,
+	io::BufReader,
+	sync::{Arc, Mutex},
+};
 
 mod command;
 mod handler;
 mod helper;
 mod model;
+
+#[derive(Debug)]
+pub struct AppState {
+	pub audio_state: Arc<Mutex<AudioState>>,
+}
+
+#[derive(Debug)]
+pub struct AudioState {
+	pub command_sender: crossbeam_channel::Sender<AudioCommands>,
+	pub playback_state: AudioPlaybackState,
+	pub current_file: Option<File>,
+}
+
+#[derive(Debug)]
+pub enum AudioCommands {
+	PlayAudioFile,
+}
 
 #[derive(Debug, Clone)]
 pub enum AudioPlaybackState {
@@ -19,19 +40,58 @@ pub enum AudioPlaybackState {
 	STOPPING,
 }
 
-#[derive(Debug, Clone)]
-pub struct AudioState {
-	pub playback_state: AudioPlaybackState,
-	pub current_file_path: String,
+fn setup_audio_thread(
+	app: &mut tauri::App,
+	state: Arc<Mutex<AudioState>>,
+	audio_event_receiver: crossbeam_channel::Receiver<AudioCommands>,
+) {
+	std::thread::spawn(move || {
+		let host = rodio::cpal::default_host();
+
+		let device = host.default_output_device().unwrap();
+
+		let (_stream, stream_handle) = rodio::OutputStream::try_from_device(&device).unwrap();
+
+		let sink = rodio::Sink::try_new(&stream_handle).unwrap();
+
+		loop {
+			while let Ok(command) = audio_event_receiver.try_recv() {
+				println!("{:?}", command);
+
+				let audio_state = state.lock().unwrap();
+
+				match command {
+					AudioCommands::PlayAudioFile => {
+						let file = audio_state.current_file.as_ref().unwrap();
+
+						sink.append(
+							rodio::Decoder::new(BufReader::new(file.try_clone().unwrap())).unwrap(),
+						);
+					}
+				}
+			}
+		}
+	});
 }
 
 fn main() -> anyhow::Result<()> {
 	// audio event protocol
-	let (audio_event_sender, audio_event_receiver) = crossbeam_channel::bounded::<AudioState>(32);
+	let (audio_command_sender, audio_command_receiver) =
+		crossbeam_channel::bounded::<AudioCommands>(32);
+
+	let audio_state = Arc::new(Mutex::new(AudioState {
+		command_sender: audio_command_sender,
+		playback_state: AudioPlaybackState::STOPPING,
+		current_file: None,
+	}));
+
+	let audio_state_cloned = audio_state.clone();
+
+	let app_state = AppState { audio_state };
 
 	// tauri
 	tauri::Builder::default()
-		.manage(audio_event_sender)
+		.manage(app_state)
 		.invoke_handler(tauri::generate_handler![
 			handler::files_event_handler::get_directory_info,
 			handler::audio_playback_handler::audio_play
@@ -42,26 +102,7 @@ fn main() -> anyhow::Result<()> {
 			// let main_window = app.get_window("main").unwrap();
 
 			// audio thread
-			std::thread::spawn(move || {
-				let host = rodio::cpal::default_host();
-
-				let device = host.default_output_device().unwrap();
-
-				let (_stream, stream_handle) =
-					rodio::OutputStream::try_from_device(&device).unwrap();
-
-				let sink = rodio::Sink::try_new(&stream_handle).unwrap();
-
-				loop {
-					while let Ok(state) = audio_event_receiver.try_recv() {
-						println!("{:?}", state);
-						let file = std::fs::File::open(state.current_file_path).unwrap();
-						sink.append(
-							rodio::Decoder::new(BufReader::new(file.try_clone().unwrap())).unwrap(),
-						);
-					}
-				}
-			});
+			setup_audio_thread(app, audio_state_cloned, audio_command_receiver);
 
 			Ok(())
 		})
