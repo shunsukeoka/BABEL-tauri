2c51f60507fb3c0f5a71a1439870f8aa30554708
Shunsuke Oka <dev@5hun.jp>
2022-07-10 04:47:18 +0900

    feat: next, prevのボタンを追加

---
+++ src/components/common/playback/NextPrevButton.stories.tsx
@@ -0,0 +1,11 @@
+import { ComponentMeta, ComponentStoryObj } from '@storybook/react'
+import { NextPrevButton } from './NextPrevButton'
+
+export default {
+    title: 'Common/PlayBack/NextPrevButton',
+    component: NextPrevButton,
+} as ComponentMeta<typeof NextPrevButton>
+
+export const Default: ComponentStoryObj<typeof NextPrevButton> = {
+    args: { color: '#000', reverse: false },
+}
