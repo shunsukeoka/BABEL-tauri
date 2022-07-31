873f15b678152dca8c03127302dde2afe5b32c5e
shunsukeoka <dev@5hun.jp>
2022-07-31 13:23:00 +0900

    feat: plopの導入

---
+++ plopfile.js
@@ -0,0 +1,8 @@
+const componentGenerator = require('./generators/component/index')
+
+/**
+ * @param {import('plop').NodePlopAPI} plop
+ */
+module.exports = function (plop) {
+    plop.setGenerator('component', componentGenerator)
+}
