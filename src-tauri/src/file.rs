7583a2a1b73c472b279a25b5733a10ad0fe25cc7
Shunsuke Oka <dev@5hun.jp>
2022-08-20 10:34:43 +0900

    refactor: implove file info serializing

--- src-tauri/src/file.rs
+++ src-tauri/src/file.rs
@@ -1,22 +1,11 @@
 use crate::helper::convert::systemtime2string;
-use anyhow::Context;
 use lofty::{AudioFile, Probe};
 use serde::Deserialize;
 use serde::Serialize;
 use std::fs;
+use std::fs::Metadata;
 use std::path::Path;
 use std::path::PathBuf;
-use std::rc::Rc;
-use thiserror::Error;
-
-#[derive(Debug, Error)]
-pub enum FileInfoError {
-	#[error("ディレクトリの読み込みに失敗しました。: {0}")]
-	ReadDirectoryError(String),
-
-	#[error("メタデータの読み込みに失敗しました。")]
-	ReadMetadataError(),
-}
 
 #[derive(Debug, Serialize, Deserialize)]
 pub struct AudioProperties {
@@ -42,135 +31,123 @@ pub struct FileInfo {
 	pub accessed_t: String,
 }
 
-pub fn read_directory(path: &String) -> anyhow::Result<Vec<FileInfo>, FileInfoError> {
+#[derive(Debug)]
+pub struct FileInfoSerializer {
+	pub path: PathBuf,
+	pub metadata: Metadata,
+}
+
+impl FileInfoSerializer {
+	pub fn new(path: &Path) -> anyhow::Result<Self> {
+		let metadata = path.metadata()?;
+
+		Ok(Self {
+			path: path.to_path_buf(),
+			metadata,
+		})
+	}
+
+	pub fn get_file_name(&self) -> String {
+		self.path
+			.file_name()
+			.map_or("".to_string(), |name| name.to_string_lossy().to_string())
+	}
+
+	pub fn get_file_size(&self) -> u64 {
+		self.metadata.len()
+	}
+
+	pub fn get_mime_type(&self) -> String {
+		mime_guess::from_path(&self.path)
+			.first_raw()
+			.map_or("".to_string(), |mime| mime.to_string())
+	}
+
+	pub fn is_directory(&self) -> bool {
+		self.metadata.is_dir()
+	}
+
+	pub fn is_file(&self) -> bool {
+		self.metadata.is_file()
+	}
+
+	pub fn is_symlink(&self) -> bool {
+		self.metadata.is_symlink()
+	}
+
+	pub fn is_readonly(&self) -> bool {
+		self.metadata.permissions().readonly()
+	}
+
+	pub fn get_created_time(&self) -> String {
+		self.metadata
+			.created()
+			.map_or("".to_string(), systemtime2string)
+	}
+
+	pub fn get_modified_time(&self) -> String {
+		self.metadata
+			.modified()
+			.map_or("".to_string(), systemtime2string)
+	}
+
+	pub fn get_accessed_time(&self) -> String {
+		self.metadata
+			.accessed()
+			.map_or("".to_string(), systemtime2string)
+	}
+
+	pub fn create_audio_properties(&self) -> Option<AudioProperties> {
+		Probe::open(&self.path).map_or(None, |tagged_file| {
+			tagged_file.read(true).map_or(None, |tag| {
+				let properties = tag.properties();
+
+				Some(AudioProperties {
+					channels: properties.channels(),
+					bit_depth: properties.bit_depth(),
+					sample_rate: properties.sample_rate(),
+					duration: properties.duration().as_secs_f64(),
+				})
+			})
+		})
+	}
+
+	pub fn serialize(&self) -> FileInfo {
+		FileInfo {
+			file_path: self.path.clone(),
+			file_name: self.get_file_name(),
+			file_size: self.get_file_size(),
+			mime: self.get_mime_type(),
+			is_dir: self.is_directory(),
+			is_file: self.is_file(),
+			is_symlink: self.is_symlink(),
+			readonly: self.is_readonly(),
+			audio_properties: self.create_audio_properties(),
+			created_t: self.get_created_time(),
+			modified_t: self.get_modified_time(),
+			accessed_t: self.get_accessed_time(),
+		}
+	}
+}
+
+pub fn read_directory(path: &String) -> Vec<FileInfo> {
 	let mut file_info_vec = vec![];
 
-	if let Ok(entries) =
-		fs::read_dir(path).context(FileInfoError::ReadDirectoryError(path.to_string()))
-	{
-		for entry in entries {
-			if let Ok(entry) = entry {
-				let entry_ptr = Rc::new(&entry);
-
-				let metadata = entry_ptr
-					.metadata()
-					.context(FileInfoError::ReadMetadataError())
-					.unwrap();
-
-				let file_info = FileInfo {
-					file_path: entry_ptr.path(),
-					file_name: entry_ptr
-						.file_name()
-						.into_string()
-						.unwrap_or_else(|_| "".to_string()),
-					file_size: metadata.len(),
-					mime: mime_guess::from_path(entry_ptr.path())
-						.first_raw()
-						.unwrap_or("")
-						.to_string(),
-					is_dir: metadata.is_dir(),
-					is_file: metadata.is_file(),
-					is_symlink: metadata.is_symlink(),
-					readonly: metadata.permissions().readonly(),
-					audio_properties: create_audio_properties(entry_ptr.path().as_path()),
-					created_t: systemtime2string(
-						metadata
-							.created()
-							.context(FileInfoError::ReadMetadataError())
-							.unwrap(),
-					),
-					modified_t: systemtime2string(
-						metadata
-							.modified()
-							.context(FileInfoError::ReadMetadataError())
-							.unwrap(),
-					),
-					accessed_t: systemtime2string(
-						metadata
-							.accessed()
-							.context(FileInfoError::ReadMetadataError())
-							.unwrap(),
-					),
-				};
-
+	if let Ok(entries) = fs::read_dir(path) {
+		for entry in entries.flatten() {
+			let _ = FileInfoSerializer::new(&entry.path()).map(|serializer| {
+				let file_info = serializer.serialize();
 				file_info_vec.push(file_info);
-			} else {
-				Err(FileInfoError::ReadDirectoryError(path.to_string()))?;
-			}
+			});
 		}
-	} else {
-		Err(FileInfoError::ReadDirectoryError(path.to_string()))?;
-	};
-
-	Ok(file_info_vec)
-}
-
-pub fn read_file(path: &String) -> anyhow::Result<FileInfo, FileInfoError> {
-	let entry = Path::new(path);
-
-	let metadata = entry
-		.metadata()
-		.context(FileInfoError::ReadMetadataError())
-		.unwrap();
-
-	let file_info = FileInfo {
-		file_path: entry.to_path_buf(),
-		file_name: entry
-			.file_name()
-			.unwrap()
-			.to_os_string()
-			.into_string()
-			.unwrap_or_else(|_| "".to_string()),
-		file_size: metadata.len(),
-		mime: mime_guess::from_path(entry)
-			.first_raw()
-			.unwrap_or("")
-			.to_string(),
-		is_dir: metadata.is_dir(),
-		is_file: metadata.is_file(),
-		is_symlink: metadata.is_symlink(),
-		readonly: metadata.permissions().readonly(),
-		audio_properties: create_audio_properties(entry),
-		created_t: systemtime2string(
-			metadata
-				.created()
-				.context(FileInfoError::ReadMetadataError())
-				.unwrap(),
-		),
-		modified_t: systemtime2string(
-			metadata
-				.modified()
-				.context(FileInfoError::ReadMetadataError())
-				.unwrap(),
-		),
-		accessed_t: systemtime2string(
-			metadata
-				.accessed()
-				.context(FileInfoError::ReadMetadataError())
-				.unwrap(),
-		),
-	};
-
-	Ok(file_info)
-}
-
-fn create_audio_properties(path: &Path) -> Option<AudioProperties> {
-	if let Ok(tagged_file) = Probe::open(path) {
-		if let Ok(tag) = tagged_file.read(true) {
-			let properties = tag.properties();
-
-			Some(AudioProperties {
-				channels: properties.channels(),
-				bit_depth: properties.bit_depth(),
-				sample_rate: properties.sample_rate(),
-				duration: properties.duration().as_secs_f64(),
-			})
-		} else {
-			None
-		}
-	} else {
-		None
 	}
+
+	file_info_vec
+}
+
+pub fn read_file(path: &String) -> Option<FileInfo> {
+	FileInfoSerializer::new(&PathBuf::from(path))
+		.map_or(None, |serializer| Some(serializer.serialize()))
 }
 
 #[derive(Debug, Serialize)]
@@ -182,40 +159,22 @@ pub struct Response<T> {
 
 #[tauri::command]
 pub fn get_directory_info(path: String) -> Response<Vec<FileInfo>> {
-	match read_directory(&path) {
-		Err(msg) => {
-			return Response {
-				success: false,
-				payload: None,
-				err_msg: Some(msg.to_string()),
-			}
-		}
-		Ok(file_info_vec) => {
-			return Response {
-				success: true,
-				payload: Some(file_info_vec),
-				err_msg: None,
-			}
-		}
+	let file_info_vec = read_directory(&path);
+
+	Response {
+		success: true,
+		payload: Some(file_info_vec),
+		err_msg: None,
 	}
 }
 
 #[tauri::command]
 pub fn add_directory(path: String) -> Response<FileInfo> {
-	match read_file(&path) {
-		Err(msg) => {
-			return Response {
-				success: false,
-				payload: None,
-				err_msg: Some(msg.to_string()),
-			}
-		}
-		Ok(file_info) => {
-			return Response {
-				success: true,
-				payload: Some(file_info),
-				err_msg: None,
-			}
-		}
+	let file_info = read_file(&path);
+
+	Response {
+		success: true,
+		payload: file_info,
+		err_msg: None,
 	}
 }
