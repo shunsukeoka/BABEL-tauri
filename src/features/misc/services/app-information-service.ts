2035e9ce3f46c74540edd0e9d4ee372ee19b752d
Shunsuke Oka <dev@5hun.jp>
2023-01-01 06:22:09 +0900

    feat: install tsyringe and reflect-metadata

--- src/features/misc/services/app-information-service.ts
+++ src/features/misc/services/app-information-service.ts
@@ -1,13 +1,24 @@
-import { IAppInformationRepository, TauriAppInformationRepository } from '../repositories'
+import 'reflect-metadata'
+import { container, inject, injectable } from 'tsyringe'
+import type { IAppInformationRepository } from '../repositories'
+import { TauriAppInformationRepository } from '../repositories'
 
+const INJECTION_TOKEN = Symbol('IAppInformationRepository')
+
+@injectable()
 class AppInformationService {
-    constructor(private repository: IAppInformationRepository) {}
+    constructor(
+        @inject(INJECTION_TOKEN)
+        private readonly repository: IAppInformationRepository,
+    ) {}
 
     public async getAppVersion(): Promise<string> {
         return this.repository.getAppVersion()
     }
 }
 
-const appInformationService = new AppInformationService(new TauriAppInformationRepository())
+container.register(INJECTION_TOKEN, {
+    useClass: TauriAppInformationRepository,
+})
 
-export { appInformationService }
+export const appInformationService = container.resolve(AppInformationService)
